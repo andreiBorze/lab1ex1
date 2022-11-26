59ec7d8861cc2cb9a21b8b62d59dfd8b80bd80e5
Borze Andrei <borzeandrew@gmail.com>
2022-11-26 20:47:05 +0200

    Add project files.

---
+++ lab1ex1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lab1ex1
+{
+    /*Scrieti un program care citind de la tastaura cele trei dimensiuni ale unui paralelipiped dreptunghic, va afisa volumul lui*/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Acest program va calcula volumul unui paralepiped. \n");
+
+            Console.WriteLine("Introduceti dimensiunile laturii 1:");
+            var latura1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti dimensiunile laturii 2:");
+            var latura2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti dimensiunile laturii 3:");
+            var latura3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Volulmul paralepipedului este: " + latura1* latura2* latura3);
+          
+        }
+
+    }
+}
